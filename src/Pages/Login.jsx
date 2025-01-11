190e60a51e47a6bef0c7853430ca96d621b7ce23
Aadarsh Narayan PS <158171578+AadarshNarayan@users.noreply.github.com>
2025-01-11 23:06:57 +0530

    Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3001/login", { email, password })
+      .post("https://library-backend-1-1qrz.onrender.com/login", { email, password })
       .then((result) => {
         console.log(result);
 
