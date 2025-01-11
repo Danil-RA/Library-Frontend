eba12556d3dac8d9f61e88f13842d5a4b2db4112
Aadarsh Narayan PS <158171578+AadarshNarayan@users.noreply.github.com>
2025-01-11 23:05:54 +0530

    Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -41,7 +41,7 @@ const Signup = () => {
   const [errorMessage, setErrorMessage] = useState("")
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3001/signup', { name, place, age, email, password, education, contact, phone,NoOfBooks: 0 })
+    axios.post('https://library-backend-1-1qrz.onrender.com/signup', { name, place, age, email, password, education, contact, phone,NoOfBooks: 0 })
       .then(result => {
         console.log(result)
         navigate('/login')
